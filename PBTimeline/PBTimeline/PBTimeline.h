0e5b7d5b97eed7620b013cfb75bf350e53ba2d09
pabloblancoopinno <pablo.blanco@opinno.com>
2020-04-11 11:05:50 +0200

    Create project

---
+++ PBTimeline/PBTimeline/PBTimeline.h
@@ -0,0 +1,19 @@
+//
+//  PBTimeline.h
+//  PBTimeline
+//
+//  Created by Pablo Blanco González on 11/04/2020.
+//  Copyright © 2020 PabloBlanco. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PBTimeline.
+FOUNDATION_EXPORT double PBTimelineVersionNumber;
+
+//! Project version string for PBTimeline.
+FOUNDATION_EXPORT const unsigned char PBTimelineVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PBTimeline/PublicHeader.h>
+
+
